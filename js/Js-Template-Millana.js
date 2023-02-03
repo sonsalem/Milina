8a35b9709df10591e66fb07f8463297aef0f3c17
SonSalem <salemmamdouh77777@gmail.com>
2023-02-03 16:32:29 +0200

    Uploding Main Files

---
+++ js/Js-Template-Millana.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-banner").owlCarousel({
+    loop:true,
+    items: 1,
+    nav:true,
+    autoplayTimeout:4000,
+    autoplay: true,
+    items:1,
+    }
+  );
+});
